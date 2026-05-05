679bc5f7c02b4a13faa5fd589d6a8dd80a3455cc
metiallen027@gmail.com <allen@example.com>
2026-05-05 05:11:28 +0200

    update CORS for production

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Basic middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://re-mmogo.vercel.app'],
+  origin: ['http://localhost:5173', 'https://re-mmogo-rho.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
